0ea04cfb9275b3f35cace69d53f327bfba03bc54
deepak104080 <deepak104080@gmail.com>
2022-08-03 23:32:40 +0530

    3rd August 2022 - Session

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require("dotenv").config({ path: ".env" });
+
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    try {
+        console.log('ExamHub API is running.........');
+        res.send('<h1>ExamHub API is running.........</h1>');
+    }
+    catch{
+
+    }
+})
+
+const usersRoute = require('./routes/users');
+app.use('/users', usersRoute);
+
+
+
+app.listen(process.env.APP_PORT);
---
+++ routes/users.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const router = express.Router();
+
+const db = require('../db');
+
+//http://localhost:4000/users/allusers
+router.get('/allusers', async(req, res) => {
+    try{
+        const response = await db.promise().query('SELECT * FROM users');
+        //console.log(response[0]);
+        res.status(200).json(response[0]);
+    }
+    catch(err){
+        res.status(400).json(err);
+    }
+})
+
+
+module.exports = router;

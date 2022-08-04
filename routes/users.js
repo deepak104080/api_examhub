273458d2613b677be64127fbc322447a5c7529e0
deepak104080 <deepak104080@gmail.com>
2022-08-04 23:52:36 +0530

    4th August 2022 - Session

--- routes/users.js
+++ routes/users.js
@@ -16,5 +16,46 @@ router.get('/allusers', async(req, res) => {
     }
 })
 
+//http://localhost:4000/users/searchuser/:id
+
+router.get("/searchuser/:userId", async (req, res) => {
+    try {
+        const userId = req.params.userId;
+        const response = await db
+            .promise()
+            .query(`SELECT name, email, username FROM users WHERE userid = '${userId}'`);
+        res.status(200).json(response[0]);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// http://localhost:4000/users/finduser?id=id005
+router.get("/finduser/", async (req, res) => {
+    try {
+        const userId = req.query.id;
+        const response = await db
+            .promise()
+            .query(`SELECT * FROM users WHERE userid = '${userId}'`);
+        res.status(200).json(response[0]);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// http://localhost:4000/users/removeuser/id004
+router.delete("/removeuser/:userId", async (req, res) => {
+    try {
+        const userId = req.params.userId;
+        const response = await db
+            .promise()
+            .query(`DELETE FROM users WHERE userid = '${userId}'`);
+        res.status(200).json(response[0]);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+  
+
 
 module.exports = router;

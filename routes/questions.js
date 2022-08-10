2d1e844c61ee190b4952fce6dfbc016011394105
deepak104080 <deepak104080@gmail.com>
2022-08-11 02:16:29 +0530

    10th August 2022 - Session

--- routes/questions.js
+++ routes/questions.js
@@ -16,46 +16,45 @@ router.get('/getquestions', async(req, res) => {
     }
 })
 
-//http://localhost:4000/users/searchuser/:id
 
-// router.get("/searchuser/:userId", async (req, res) => {
-//     try {
-//         const userId = req.params.userId;
-//         const response = await db
-//             .promise()
-//             .query(`SELECT name, email, username FROM users WHERE userid = '${userId}'`);
-//         res.status(200).json(response[0]);
-//     } catch (err) {
-//         res.status(400).json(err);
-//     }
-// });
+//http://localhost:4000/questions/getquestionbysubject/subject_id
+router.get("/getquestionbysubject/:subject_id", async (req, res) => {
+    try {
+        const subject_id = req.params.subject_id;
+        // if caching exits - fetch from caching - memcache/redis
+        // else make a db call
+        const response = await db
+            .promise()
+            .query(`SELECT * FROM questions WHERE subject_id = '${subject_id}'`);
+        res.status(200).json(response[0]);
+        // caching. save - response[0]
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
-// http://localhost:4000/users/finduser?id=id005
-// router.get("/finduser/", async (req, res) => {
-//     try {
-//         const userId = req.query.id;
-//         const response = await db
-//             .promise()
-//             .query(`SELECT * FROM users WHERE userid = '${userId}'`);
-//         res.status(200).json(response[0]);
-//     } catch (err) {
-//         res.status(400).json(err);
-//     }
-// });
+// http://localhost:4000/questions/searchquestion/qs_id
+router.get("/searchquestion/:qs_id", async (req, res) => {
+    try {
+        const qs_id = req.params.qs_id;
+        const response = await db.promise().query(`SELECT * FROM questions WHERE qs_id = '${qs_id}'`);
+        res.status(200).json(response[0]);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
-// http://localhost:4000/users/removeuser/id004
-// router.delete("/removeuser/:userId", async (req, res) => {
-//     try {
-//         const userId = req.params.userId;
-//         const response = await db
-//             .promise()
-//             .query(`DELETE FROM users WHERE userid = '${userId}'`);
-//         res.status(200).json(response[0]);
-//     } catch (err) {
-//         res.status(400).json(err);
-//     }
-// });
-
+// http://localhost:4000/questions/getquestionbysubject/?subject=1&count=2
+router.get("/getquestionbysubject/", async (req, res) => {
+    try {
+        const subject_id = req.query.subject;
+        const count = req.query.count;
+        const response = await db.promise().query(`SELECT * FROM questions WHERE subject_id = '${subject_id}' ORDER BY RAND() LIMIT ${count} `);
+        res.status(200).json(response[0]);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 // http://localhost:4000/questions/addquestion
 router.post('/addquestion', async(req, res) => {
@@ -72,45 +71,34 @@ router.post('/addquestion', async(req, res) => {
 })
   
 
+// http://localhost:4000/questions/editquestion
+router.put('/editquestion', async(req, res) => {
+    try {
+        console.log(req.body);
+        const response = await db.promise().query(`UPDATE questions SET question = '${req.body.question}', 
+        answer1 = '${req.body.answer1}', answer2 = '${req.body.answer2}', 
+        answer3 = '${req.body.answer3}', answer4 = '${req.body.answer4}', 
+        answer5 = '${req.body.answer5}', correctanswer = '${req.body.correctanswer}' 
+        WHERE qs_id = '${req.body.qs_id}'`);
+        console.log(response);
+        res.status(200).json(response[0]);
+    } catch(err) {
+        // console.log(err);
+        res.status(400).json({ massage: err.massage });
+    }
+})
 
-// http://localhost:4000/users/login
-// router.post('/login', async(req, res) => {
-//     try {
-//         const response = await db.promise().query(`SELECT * FROM users WHERE username = '${req.body.username}' `);
-//         // user found in db
-//         if(response[0].length > 0) {
-//             //password matched
-//             // console.log(response[0][0].password, req.body.password);
-//             if(response[0][0].password == req.body.password) {
-//                 // res.status(202).json(response[0]);
-//                 res.status(202).json({message: 'Successfully logged in'});
-//             }
-//             //password not matched
-//             else {
-//                 res.status(401).json({message: 'Incorrect Password'});
-//             }
-//         }
-//         // user not found
-//         else {
-//             res.status(422).json({message: 'User Not Found'});
-//         }        
-//     } catch(err) {
-//         // console.log(err);
-//         res.status(400).json(err);
-//     }
-// })
-
-// http://localhost:4000/users/updateuserpassword
-// router.put('/updateuserpassword', async(req, res) => {
-//     try {
-//         console.log(req.body);
-//         const response = await db.promise().query(`UPDATE users SET password = '${req.body.password}' WHERE username = '${req.body.username}'`);
-//         console.log(response);
-//         res.status(200).json(response[0]);
-//     } catch(err) {
-//         // console.log(err);
-//         res.status(400).json({ massage: err.massage });
-//     }
-// })
+// http://localhost:4000/questions/removequestion/qs_id
+router.delete("/removequestion/:qs_id", async (req, res) => {
+    try {
+        const qs_id = req.params.qs_id;
+        const response = await db
+            .promise()
+            .query(`DELETE FROM questions WHERE qs_id = '${qs_id}'`);
+        res.status(200).json(response[0]);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 module.exports = router;
